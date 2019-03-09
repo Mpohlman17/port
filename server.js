a85f377ae4c331ac9f92c4ca254a0d2db08197eb
Michael Pohlman <michael.pohlman1@gmail.com>
2019-03-09 13:02:25 -0500

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession =require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys.js");
 
-require('./models/User.js');
-require('./services/passport');
+require("./models/User.js");
+require("./services/passport");
 
 const app = express();
 
@@ -20,20 +20,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/rand-o",
-  {
-    useMongoClient: true
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rand-o", {
+  useMongoClient: true
+});
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,17 @@
-const passport = require('passport');
-const GitHubStrategy = require('passport-github').Strategy;
-const mongoose = require('mongoose');
-const User = mongoose.model('users');
-const keys = require('../config/keys');
+const passport = require("passport");
+const GitHubStrategy = require("passport-github").Strategy;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../config/keys");
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
-})
+});
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    })
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
@@ -21,7 +20,7 @@ passport.use(
       clientID: keys.githubClientID,
       clientSecret: keys.githubClientSecret,
       callbackURL: keys.githubCallbackURL,
-      proxy: true
+      proxy: false
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ githubId: profile.id });

b012bb75ebb6406797ec3cf505421db4424838df
Michael Pohlman <michael.pohlman1@gmail.com>
2019-03-09 11:22:36 -0500

    push

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import configureStore from "./configureStore";
-import AppContainer from "./components/AppContainer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-const store = configureStore();
+import App from './App';
+import reducers from './reducers';
+
+// import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <AppContainer />
-  </Provider>,
-  document.getElementById("root")
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
 );
+
+// registerServiceWorker();

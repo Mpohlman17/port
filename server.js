ee6cd8ecacdb689a5f8c8cd6c633aab41165e9a1
Michael Pohlman <michael.pohlman1@gmail.com>
2019-03-11 21:02:18 -0400

    evil heroku

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+// hey hey hey
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
